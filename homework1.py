50938c7c7dd76eb02925ea9242a6b477213ee9c6
Samuel Amoabeng <amoabengs1986@utexas.edu>
2023-01-19 12:57:50 -0600

    second commit

---
+++ homework1.py
@@ -0,0 +1,19 @@
+
+
+
+
+words = []
+sorted_words = []
+
+
+with open('words', 'r') as f:
+        words = f.read().splitlines()
+
+
+words.sort(key=len, reverse=True)
+
+
+for i in range(5):
+        print(words[i])
+    
+
